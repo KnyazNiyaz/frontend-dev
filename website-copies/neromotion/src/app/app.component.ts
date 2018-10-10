ee1ffd064331127fb2f654f6f3fe4397a16dcbe5
Emil Niyaz <niyaz.emil@gmail.com>
2018-10-09 23:03:01 -0400

    First step of portfolio

--- website-copies/neromotion/src/app/app.component.ts
+++ website-copies/neromotion/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, HostListener, ViewChild} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from "@angular/core";
 
 declare var $: any;
 
@@ -7,26 +7,36 @@ declare var $: any;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  newWidth: number;
-  opacity: number;
+export class AppComponent implements OnInit {
+  newWidth = 50;
+  isClicked = false;
+
 
   @ViewChild('bg') elem;
 
   @HostListener('window:scroll', ['$event']) onWindowScroll(event) {
-    const scrollTo = window.pageYOffset;
-    const docHeight = document.documentElement.scrollHeight;
-    const scrollHeight = this.elem.nativeElement.offsetHeight;
-    const scrollPercent = (scrollTo / (docHeight - scrollHeight)) * 100;
-    this.newWidth = 50 + (scrollPercent * 2);
+      const scrollTo = window.pageYOffset;
+      const docHeight = document.documentElement.scrollHeight;
+      const scrollHeight = this.elem.nativeElement.offsetHeight;
+      const scrollPercent = (scrollTo / (docHeight - scrollHeight)) * 100;
+      this.newWidth = 50 + (scrollPercent * 2);
+      const calc = 1 - ((scrollTo - 150) / 100);
+      this.elem.nativeElement.style.opacity = calc;
+      if (calc > 1) {
+        this.elem.nativeElement.style.opacity = 1;
+      } else if (calc < 0) {
+        this.elem.nativeElement.style.opacity = 0;
+      }
+  }
 
-    const calc = 1 - ((scrollTo - 150) / 100);
-    this.elem.nativeElement.style.opacity = calc;
-    if (calc > 1) {
-      this.elem.nativeElement.style.opacity = 1;
-    } else if (calc < 0) {
-      this.elem.nativeElement.style.opacity = 0;
-    }
+  ngOnInit() {
+    document.body.style.overflow = 'hidden';
+  }
+
+
+  disappear() {
+    document.body.style.overflow = 'auto';
+    this.isClicked = true;
   }
 
 }
