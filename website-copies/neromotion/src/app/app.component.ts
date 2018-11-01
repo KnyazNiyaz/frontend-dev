ed29162da7fa1dad6b6928a5b2887126c7aad6e4
Emil Niyaz <niyaz.emil@gmail.com>
2018-10-31 23:46:44 -0400

    Changed the flow

--- website-copies/neromotion/src/app/app.component.ts
+++ website-copies/neromotion/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, HostListener, OnInit, ViewChild} from "@angular/core";
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { ScrollToConfigOptions, ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
 
 declare var $: any;
 
@@ -10,33 +11,45 @@ declare var $: any;
 export class AppComponent implements OnInit {
   newWidth = 50;
   isClicked = false;
-
+  isOpen = false;
+  isTrue = false;
 
   @ViewChild('bg') elem;
+  @ViewChild('main') main;
+
+  constructor(private _scrollToService: ScrollToService) {
+  }
 
   @HostListener('window:scroll', ['$event']) onWindowScroll(event) {
-      const scrollTo = window.pageYOffset;
-      const docHeight = document.documentElement.scrollHeight;
-      const scrollHeight = this.elem.nativeElement.offsetHeight;
-      const scrollPercent = (scrollTo / (docHeight - scrollHeight)) * 100;
-      this.newWidth = 50 + (scrollPercent * 2);
-      const calc = 1 - ((scrollTo - 150) / 100);
-      this.elem.nativeElement.style.opacity = calc;
-      if (calc > 1) {
-        this.elem.nativeElement.style.opacity = 1;
-      } else if (calc < 0) {
-        this.elem.nativeElement.style.opacity = 0;
-      }
+    const scrollTo = window.pageYOffset;
+    const docHeight = document.documentElement.scrollHeight;
+    const scrollHeight = this.elem.nativeElement.offsetHeight;
+    const scrollPercent = (scrollTo / (docHeight - scrollHeight)) * 100;
+    this.newWidth = 50 + (scrollPercent * 2);
+    const calc = 1 - ((scrollTo - 250) / 100);
+    this.elem.nativeElement.style.opacity = calc;
+    if (calc > 1) {
+      this.elem.nativeElement.style.opacity = 1;
+      this.main.nativeElement.style.zIndex = -1;
+    } else if (calc < 0) {
+      this.elem.nativeElement.style.opacity = 0;
+      this.main.nativeElement.style.zIndex = 1;
+    }
   }
 
   ngOnInit() {
-    document.body.style.overflow = 'hidden';
   }
 
+  public triggerScrollToOffsetOnly(offset: number = 0) {
+    this.isOpen = true;
+    // this.isClicked = true;
+    this.isTrue = true;
+    const config: ScrollToConfigOptions = {
+      duration: 7000,
+      easing: 'easeOutElastic',
+      offset
+    };
 
-  disappear() {
-    document.body.style.overflow = 'auto';
-    this.isClicked = true;
+    this._scrollToService.scrollTo(config);
   }
-
 }
--- website-copies/neromotion/src/app/app.module.ts
+++ website-copies/neromotion/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ScrollToModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
