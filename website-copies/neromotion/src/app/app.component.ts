4a7e0e75bcf5df7b12ee12ec022eef8ad7346ea8
Emil Niyaz <niyaz.emil@gmail.com>
2018-11-10 17:45:50 -0500

    Last section structure

--- website-copies/neromotion/src/app/app.component.ts
+++ website-copies/neromotion/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ScrollToConfigOptions, ScrollToService } from '@nicky-lenaers/ngx-scroll-to';
 
 declare var $: any;
@@ -13,24 +13,31 @@ export class AppComponent implements OnInit {
   isClicked = false;
   isOpen = false;
   isTrue = false;
+  oldScroll;
 
   @ViewChild('bg') elem;
   @ViewChild('main') main;
 
-  constructor(private _scrollToService: ScrollToService) {
+  constructor(private _scrollToService: ScrollToService,
+              private elementRef: ElementRef) {
+    document.body.style.backgroundColor = 'black';
   }
 
   @HostListener('window:scroll', ['$event']) onWindowScroll(event) {
     const scrollTo = window.pageYOffset;
-    const docHeight = document.documentElement.scrollHeight;
     const scrollHeight = this.elem.nativeElement.offsetHeight;
-    const scrollPercent = (scrollTo / (docHeight - scrollHeight)) * 100;
+    const scrollPercent = (scrollTo / (this.getHeight() - scrollHeight)) * 100;
+
     this.newWidth = 50 + (scrollPercent * 2);
     const calc = 1 - ((scrollTo - 250) / 100);
     this.elem.nativeElement.style.opacity = calc;
+    if ((this.oldScroll > window.scrollY) && calc > 0) {
+      this.main.nativeElement.style.zIndex = -1;
+    }
+    this.oldScroll = window.scrollY;
+
     if (calc > 1) {
       this.elem.nativeElement.style.opacity = 1;
-      this.main.nativeElement.style.zIndex = -1;
     } else if (calc < 0) {
       this.elem.nativeElement.style.opacity = 0;
       this.main.nativeElement.style.zIndex = 1;
@@ -40,16 +47,30 @@ export class AppComponent implements OnInit {
   ngOnInit() {
   }
 
+  getHeight() {
+    const D = document;
+    return Math.max(Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+      Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+      Math.max(D.body.clientHeight, D.documentElement.clientHeight));
+  }
+
   public triggerScrollToOffsetOnly(offset: number = 0) {
+    this.elementRef.nativeElement.ownerDocument.body.style.background = 'none';
     this.isOpen = true;
-    // this.isClicked = true;
+    this.isClicked = true;
     this.isTrue = true;
     const config: ScrollToConfigOptions = {
-      duration: 7000,
-      easing: 'easeOutElastic',
+      duration: 1000,
+      // easing: 'easeOutElastic',
       offset
     };
 
     this._scrollToService.scrollTo(config);
   }
+
+
+  trig() {
+      this.triggerScrollToOffsetOnly(800);
+  }
+
 }
