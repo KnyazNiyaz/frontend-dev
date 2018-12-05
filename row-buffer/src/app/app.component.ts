614b519d3682f1af21bbb982fc83b30a341ed980
nuulu143 <Niiaz_uulu@comcast.com>
2018-12-05 17:23:58 -0500

    1 step of row buffer

--- row-buffer/src/app/app.component.ts
+++ row-buffer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'row-buffer';
+  @ViewChild('table') table;
+
+  @HostListener('scroll', ['$event'])
+  public scrolled($event: Event) {
+    console.log($event);
+  }
+
+  arr = [['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12],
+    ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12],
+    ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12], ['asd', 12]];
+
+  onScroll(e) {
+    console.log(e);
+    // console.log(e.getBoundingClientRect());
+  }
+
+  check() {
+    console.log(this.table.nativeElement.getBoundingClientRect());
+  }
 }
